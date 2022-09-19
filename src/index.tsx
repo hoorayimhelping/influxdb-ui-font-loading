acd7ee572465b8f0678c767e39feb6e18ebfb423
Bucky Schwarz <d.w.schwarz@gmail.com>
2022-09-19 16:16:38 -0400

    add clockface nav

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
-// import ReactDOM from 'react-dom/client';
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom/client'
 
-import {AppWrapper, Page} from '@influxdata/clockface'
+import {AppWrapper, Icon, IconFont, TreeNav, Page} from '@influxdata/clockface'
 import '@influxdata/clockface/dist/index.css'
+
+const Navigation = () => {
+  const [isExpanded, setIsExpanded] = useState(true)
+
+  const handleExpansionToggle = () => {
+    setIsExpanded(!isExpanded)
+  }
+
+  return (
+    <TreeNav
+      expanded={isExpanded}
+      onToggleClick={handleExpansionToggle}
+    >
+      <TreeNav.Item
+        label="Upload"
+        icon={<Icon glyph={IconFont.Upload_New} />}
+      />
+      <TreeNav.Item
+        label="Graphs"
+        icon={<Icon glyph={IconFont.GraphLine_New} />}
+      />
+    </TreeNav>
+  )
+}
 
 const App = () => {
   return (
     <AppWrapper classname="yes">
+      <Navigation />
       <Page>
         <Page.Contents>
           <h1>Hello, world!</h1>
+          <h1 style={{'fontFamily': 'Helvetica'}}>Hello, non-styled world!</h1>
         </Page.Contents>
       </Page>
     </AppWrapper>
@@ -19,5 +44,4 @@ const App = () => {
 
 console.log('hello!')
 
-// ReactDOM.createRoot(document.getElementById('content')).render(<App />)
-ReactDOM.render(<App />, document.getElementById('content'))
+ReactDOM.createRoot(document.getElementById('content')).render(<App />)
